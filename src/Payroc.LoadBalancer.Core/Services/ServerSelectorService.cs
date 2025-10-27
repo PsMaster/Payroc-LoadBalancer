9d50513e3bde21e48111ee1f547205bc3ed988ae
Gintaras Bubelevicius <gintaras.bubelevicius@futureplatforms.com>
2025-10-27 15:20:35 +0000

    Round robin next server retrieval fix

--- src/Payroc.LoadBalancer.Core/Services/ServerSelectorService.cs
+++ src/Payroc.LoadBalancer.Core/Services/ServerSelectorService.cs
@@ -5,7 +5,7 @@ namespace Payroc.LoadBalancer.Core.Services
 {
     public class ServerSelectorService : IServerSelectorService
     {
-        
+        private long _counter = 0;
         public ServerSelectorService()
         {
             
@@ -16,19 +16,25 @@ namespace Payroc.LoadBalancer.Core.Services
 
         public IPEndPoint? GetNextServer(ServerState currentState)
         {
-            var leastBusyHealthyServer = currentState.ServerStateDictionary.Where(x => x.Value.IsHealthy)
-                .OrderBy(x => x.Value.Usage.RequestsServed).FirstOrDefault();
-            if (leastBusyHealthyServer.Key == null)
+            var healthyServers = currentState.ServerStateDictionary.Where(x => x.Value.IsHealthy).Select(x => x.Key).ToList();
+            if (!healthyServers.Any())
             {
                 return null;
             }
 
-            var usageValue = leastBusyHealthyServer.Value.Usage.RequestsServed+1;
+            var newCounterValue = Interlocked.Increment(ref _counter);
+            var count = healthyServers.Count;
+            var index = (int)((newCounterValue - 1) % count);
+            var ipEndpoint = healthyServers[index];
+            var valueRetrieved = currentState.ServerStateDictionary.TryGetValue(ipEndpoint, out var selectedServer);
+            if (!valueRetrieved) return ipEndpoint;
+            var usageValue = selectedServer!.Usage.RequestsServed + 1;
 
-            currentState.ServerStateDictionary.TryUpdate(leastBusyHealthyServer.Key,
-                leastBusyHealthyServer.Value with { Usage = leastBusyHealthyServer.Value.Usage with { RequestsServed = usageValue } },
-                leastBusyHealthyServer.Value);
-            return leastBusyHealthyServer.Key;
+            currentState.ServerStateDictionary.TryUpdate(ipEndpoint,
+                selectedServer with { Usage = selectedServer.Usage with { RequestsServed = usageValue } },
+                selectedServer);
+
+            return ipEndpoint;
         }
     }
 }
